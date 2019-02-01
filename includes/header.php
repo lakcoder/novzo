f17d600e2154221948d09a1817771d6a6e7a81f5
root <anillakshya6@gmail.com>
2019-02-01 19:23:49 +0530

    structured

--- includes/header.php
+++ includes/header.php
@@ -1,35 +1,6 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html>
-<!--<header id="header" role="banner">
-  <div class="container">
-    <div class="row align-items-center justify-content-between d-flex">
-      <div id="logo">
-        <a href="index.php"><img src="img/logos/tt2.png" alt="Novzo Logo" height="55.76" width ="100" /></a>
-      </div>
-      <nav id="nav-menu-container" class="main-nav">
-        <ul class="nav-menu">
-          <li class="menu-active"><a href="index.php">Home</a></li>
-          <li class="menu-active"><a href="about.php">Who we are</a></li>-->
-          <!-- <li class="menu-has-children"><a href="">Services</a>
-            <ul>
-              <li><a href="blog-home.html">Buy/ Sell Books</a></li>
-              <li><a href="blog-single.html">Exchange/ Share Books</a></li>
-              <li><a href="blog-single.html">Exchange/ Share Books</a></li>
-            </ul>
-          </li> -->
-          <!--<li class="menu-active"><a href="donate.php">Donate Books</a></li>
-          <?php  // if (isset($_SESSION['username'])) { ?>
-          <li class="menu-active"><a href="dashboard.php">Dashboard</a></li>
-          <li class="menu-active"><a href="regphp/logout.php">Logout</a></li>
-          <?php //}else{ ?>
-          <li class="menu-active"><a class="cd-signin hov" href="#0">Login/ SignUp</a></li>
-          <?php //} ?>
-        </ul>
-      </nav>
-    </div>
-  </div>
-</header>-->
 <header id="header" role="banner">
 <nav class="nav">
         <div class="container">
@@ -68,7 +39,7 @@
     </nav>
   </header>
 	<!-- Jquery needed -->
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> 
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <!--<script src="js/scripts.js"></script>-->
 
 <!-- Function used to shrink nav bar removing paddings and adding black background -->
