9536b7725e0305382f78459227227ec76cb32ba1
root <anillakshya6@gmail.com>
2019-03-03 01:23:51 +0530

    filter js

--- search.php
+++ search.php
@@ -13,13 +13,40 @@
       <div class="col-lg-3 col-3 mx auto m-3" style="border-right : 2px solid grey; border-bottom : 2px solid grey";>
         <div class="container">
           <h2>Filters</h2>
+          <form action="search.php" method="post">
           <ul>
-            <li><a href="#" class="btn btn-info" role="button">Price</a></li>
-            <li><a href="#" class="btn btn-info" role="button">Rent/ Buy</a></li>
-            <li><a href="#" class="btn btn-info" role="button">City</a></li>
-            <li><a href="#" class="btn btn-info" role="button">Newest first</a></li>
-            <li><a href="#" class="btn btn-info" role="button">New/ Second Hand</a></li>
+            <li><a id="price_but" href="#" class="btn btn-info" role="button">Price</a></li>
+            <div id="price" style="display:none">
+                <input type="number" name="min" placeholder="Min Value" />
+                <input type="number" name="max" placeholder="Max Value" />
+            </div>
+            <li><a id="type_but" href="#" class="btn btn-info" role="button">Type</a></li>
+            <div id="btype" style="display:none">
+                <div>
+                  <input type="checkbox" name="rent" value="rent" />Rent
+                  <input class="buy" type="checkbox" name="buy" value="buy" onchange="type()" />Buy
+                </div>
+                <div class="buy_res">
+                  <input type="checkbox" name="new" value="new" />New
+                  <input type="checkbox" name="sh" value="second hand" />Second Hand
+                </div>
+            </div>
+            <li><a id="city_but" href="#" class="btn btn-info" role="button">City</a></li>
+            <div id="city" style="display:none">
+                <select class="" name="city" placeholder="City">
+                  <option value="" selected disabled hidden>Choose City</option>
+                  <option value="NIT Agartala" style="color:black">Mumbai</option>
+                  <option value="NIT Allahabad" style="color:black">Nagpur</option>
+                </select>
+                <input type="submit" name="price_range" />
+            </div>
+            <li><a id="sort_but" href="#" class="btn btn-info" role="button">Sort Books</a></li>
+            <div id="sort" style="display:none">
+                <button name="newest" value="">Newest First</button>
+                <button name="oldest" value="">Oldest FIrst</button>
+            </div>
           </ul>
+        </form>
         </div>
       </div>
       <div id="feed" class="col-lg-8 col-8  mx auto m-3">
